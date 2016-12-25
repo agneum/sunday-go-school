41ec5e8f43f39cc664bdfdb778dada8ed1d04a36
Artem Kartasov <art.kartasov@gmail.com>
2016-12-25 13:20:38 +0700

    02-web-server. name processing

--- 02-web-server/main.go
+++ 02-web-server/main.go
@@ -12,5 +12,15 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Error. Bad request\n")
+		// http.Error(w, "Error. Bad request", 400)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello %s\n", name)
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
