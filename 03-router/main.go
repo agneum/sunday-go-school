662cde99d6b7d3dd148af85a6cf923cb2020b226
Artem Kartasov <art.kartasov@gmail.com>
2016-12-30 14:04:01 +0700

    03-router. Separated handler, logger has been swapped

--- 03-router/main.go
+++ 03-router/main.go
@@ -3,39 +3,35 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 
+	"github.com/Sirupsen/logrus"
+	"github.com/agneum/sunday-go-school/03-router/handlers"
 	"github.com/julienschmidt/httprouter"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	name := r.URL.Query().Get("name")
-
-	if name == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Error. Bad request\n")
-		return
-	}
-
-	fmt.Fprintf(w, "Hello %s\n", name)
+	handlers.SayHello(w, name)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	body, err := ioutil.ReadAll(r.Body)
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Error. Bad request\n")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, http.StatusText(http.StatusInternalServerError))
 	}
 
-	fmt.Fprintf(w, "Hello %s!\n", body)
+	handlers.SayHello(w, string(body))
 }
 
 func main() {
+	logrus.Infoln("Server is running..")
+
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.POST("/hello", Hello)
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	logrus.Fatal(http.ListenAndServe(":8000", router))
 }
