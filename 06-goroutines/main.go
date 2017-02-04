eb0c29ba63eff8cf989a0ffde4b85c58c50775c9
Artem Kartasov <art.kartasov@gmail.com>
2017-02-04 22:46:21 +0700

    06-routines. Base sample and ping-ponger

---
+++ 06-goroutines/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	go counter(naturals)
+	go squarer(naturals, squares)
+	go printer(squares)
+
+	var input string
+	fmt.Scanln(&input)
+}
+
+func counter(out chan int) {
+	for x := 0; ; x++ {
+		out <- x
+	}
+}
+
+func squarer(in, out chan int) {
+	for {
+		x := <-in
+		out <- x * x
+	}
+}
+
+func printer(in chan int) {
+	for {
+		fmt.Println(<-in)
+	}
+}
---
+++ 06-goroutines/ponger/ping.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func pinger(c chan string, pongChannel chan bool) {
+	for {
+		if !<-pongChannel {
+			for i := 1; i <= 2; i++ {
+				c <- "ping"
+			}
+			pongChannel <- true
+		}
+	}
+}
+
+func ponger(c chan string, pongChannel chan bool) {
+	for {
+		if <-pongChannel {
+			c <- "pong"
+			pongChannel <- false
+		}
+	}
+}
+
+func printer(c chan string, pongChannel chan bool) {
+	pongChannel <- false
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+// Run twice pinger and once ponger
+func main() {
+	var c chan string = make(chan string, 3)
+	var pongChannel chan bool = make(chan bool)
+
+	go pinger(c, pongChannel)
+	go ponger(c, pongChannel)
+	go printer(c, pongChannel)
+
+	var input string
+	fmt.Scanln(&input)
+}
